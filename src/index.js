cb83efa79aa295c822a7afe12a7d8cb03c0d9540
imagineapril <julia.timokhina604@gmail.com>
2023-08-01 14:14:46 +0300

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    n = String(Math.abs(n));
+    let result = '';
+    let i = 0;
+
+    while (i < n.length) {
+        result = `${n[i]}${result}`;
+        i = i + 1;
+    }
+
+    return Number(result);
 }
